56ffdcdecf9b5a4021482f1a89ccd5d963e0846e
imrangit07 <imranhussain21309@gmail.com>
2025-05-07 15:17:08 +0530

    Create server on Port 8080

--- FIRST/INDEX.js
+++ FIRST/INDEX.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http.createServer((req, res) => {
     res.write("<h1>This is my first node program </h1>");
-    console.log("Server is Running on Port 8080");
-
     res.end();
-}).listen(8080)
+}).listen(8080,()=>{
+    console.log("Server is Running on Port 8080");
+})
