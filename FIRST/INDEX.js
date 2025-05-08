9d7fbdf2a85f2d77fa42c4b1dfc12164691b7a5b
imrangit07 <imranhussain21309@gmail.com>
2025-05-08 14:29:42 +0530

    Add some changes in my first file

--- FIRST/INDEX.js
+++ FIRST/INDEX.js
@@ -1,8 +1,9 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-    res.write("<h1>This is my first node program </h1>");
+    res.write("<h1> This is my first node program </h1>");
+    res.write("<h2> My Name is Imran Hussain</h2>");
     res.end();
-}).listen(8080,()=>{
+}).listen(8080, () => {
     console.log("Server is Running on Port 8080");
 })
