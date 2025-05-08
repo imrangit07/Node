274576c7fccd1d8c9a7f018233d57070ecd7ca6c
imrangit07 <imranhussain21309@gmail.com>
2025-05-08 15:05:02 +0530

    Add userModule

---
+++ UserModul/Module.js
@@ -0,0 +1,17 @@
+ 
+
+ const MyName = ()=>{
+    return "<h1>Imran hussain</h1>";
+ }
+ const MyAge = ()=>{
+    return "<h1>23</h1>";
+ }
+ const MyContact = ()=>{
+    return "<h1>23803840380948</h1>";
+ }
+
+ module.exports={
+  MyName,
+  MyAge,
+  MyContact
+ }
